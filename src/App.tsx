fad799510304e9e9348951816d7d81c2859a0752
Hoangvm02 <hoangvmph13792@fpt.edu.vn>
2022-07-14 21:35:27 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import HomeLayout from './pages/layout/HomeLayout'
 import AdminLayout from './pages/layout/AdminLayout'
+import Products from './pages/admin/products/Products'
+import DashboardLayout from './componnent/Dassboard/Dassboard'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,8 +15,9 @@ function App() {
       <Route>
         <Route index element={< HomeLayout/>} />
       </Route>
-      <Route path='/admin'>
-        <Route index element={<AdminLayout/>} />
+      <Route path='/admin' element={< DashboardLayout/>}>
+        {/* <Route index element={<AdminLayout/>} /> */}
+        <Route path='products' element={< Products/>}/>
       </Route>
    </Routes>
   )
